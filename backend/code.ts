efea907d1f541c5168ce93c618d3430520789417
aktYoshitaka <sato-yoshitaka@aktio.co.jp>
2023-09-02 10:43:23 +0900

    init

---
+++ backend/code.ts
@@ -0,0 +1,18 @@
+global.echo = () => {
+  Logger.log('Hello');
+};
+
+global.doGet = () => {
+  return HtmlService.createTemplateFromFile('index')
+    .evaluate()
+    .setTitle('vue-gas-app')
+    .addMetaTag('viewport', 'width=device-width, initial-scale=1');
+};
+interface Person {
+  name: string;
+  age: number;
+}
+
+global.someFunction = (msg: string, person: Person) => {
+  // do something ...
+};
