329d7412244e71acc7dc6643fa3e799522d54ad6
yoshitaka <yoshikoro0116@icloud.com>
2023-11-03 11:47:18 +0900

    add op-sheet

---
+++ frontend/src/components/Op-Sheet.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const hour = ref(0);
+const mileage = ref(0);
+const paid = ref('無償');
+const cost = ref('金額');
+</script>
+<template>
+  <input type="text" name="opsheet" id="opsheet-hour" v-model="hour" />
+  <input type="text" name="opsheet" id="opsheet-mileage" v-model="mileage" />
+  <input type="text" name="opsheet" id="opsheet-paid" v-model="paid" />
+  <input type="text" name="opsheet" id="opsheet-cost" v-model="cost" />
+</template>
