7f0435978ffc8b7261200890eb5fb0b2f17b7d53
yoshitaka <angleryoshitaka@gmail.com>
2024-10-17 23:22:30 +0900

    re init

--- backend/init.ts
+++ backend/init.ts
@@ -9,57 +9,6 @@ export class appInit {
   static sp = SpreadsheetApp.getActiveSpreadsheet();
   static spId = this.sp.getId();
   static SETTINGSHEETINFO = {
-    DOSHEETNAME: 'do',
-    SHEETNAME: 'setting',
-    SETTINGINFOAREA: 'A:E',
-    SETTINGINFOCOLUMN: 2,
-    SETTINGINFOFOLDERCOLUMN: 1,
-    SETTINGINFOFOLDERROW: 2,
-    SETTINGINFOSTARTROW: 0,
-    TEMPFOLDER: 'A2',
-    DISTFOLDER: 'B2',
-    DATASPREADSHEETID: 'C2',
-    WEBAPPURL: 'D2',
-    FORMURL: 'E2',
-    REPORTFOLDER: 'B6',
+    //定数値を入れる
   };
-  static getSaveFolderId() {
-    const rootFolderId = appInit.sp
-      .getSheetByName(this.SETTINGSHEETINFO.SHEETNAME)
-      ?.getRange(appInit.SETTINGSHEETINFO.REPORTFOLDER)
-      .getValue();
-    return rootFolderId;
-  }
-  static getRootFolderId() {
-    const rootFolderId = appInit.sp
-      .getSheetByName(this.SETTINGSHEETINFO.SHEETNAME)
-      ?.getRange(appInit.SETTINGSHEETINFO.DISTFOLDER)
-      .getValue();
-    return rootFolderId;
-  }
-  /**
-   * getSpId
-   */
-  static getAppDataSpId() {
-    const appSpId = appInit.sp
-      .getSheetByName(this.SETTINGSHEETINFO.SHEETNAME)
-      ?.getRange(appInit.SETTINGSHEETINFO.DATASPREADSHEETID)
-      .getValue();
-    return appSpId;
-  }
-
-  static getWebAppURL() {
-    const appURL = appInit.sp
-      .getSheetByName(this.SETTINGSHEETINFO.SHEETNAME)
-      ?.getRange(appInit.SETTINGSHEETINFO.WEBAPPURL)
-      .getValue();
-    return appURL;
-  }
-  static getFormURL() {
-    const formURL = appInit.sp
-      .getSheetByName(this.SETTINGSHEETINFO.SHEETNAME)
-      ?.getRange(this.SETTINGSHEETINFO.FORMURL)
-      .getValue();
-    return formURL;
-  }
 }
